83464fa2217e5e4eb67e450d4431ce6a32c0272b
sallah21 <luckyluj24@gmail.com>
2020-02-16 11:54:40 +0100

    pug mod

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 app.set('port',process.env.PORT|| 3000);
 
 const server = app.listen(app.get("port"), () =>{
-  console.log('listening on  ${server.addres().port}');
+  console.log('listening on 3000');
 });
